f21597865bb683d3434a81ca419028248eec25c9
Sinan Ulkuatam <sku3hh@gmail.com>
2023-12-25 17:19:18 -0500

    .

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const path = require('path');
 const express = require('express');
 const marked = require('marked');
 const highlight = require('highlight.js');
-// const mustache = require('mustache');
 let mustacheExpress = require('mustache-express');
 let bodyParser = require('body-parser');
 const app = express();
@@ -26,13 +25,13 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  const markdownFilePath = path.join(__dirname, 'public/docs/python.markdoc');
+  const markdownFilePath = path.join(__dirname, 'public/docs/client/python.markdoc');
   const markdownContent = fs.readFileSync(markdownFilePath, 'utf8');
   const htmlContent = marked.parse(markdownContent);
 
   const templatePath = path.join(__dirname, 'public/index.mustache');
 
-  //     // Read tab content
+  // Read tab content
   const tab1 = fs.readFileSync(path.join(__dirname, 'public/tab1.mustache'), 'utf8');
   const tab2 = fs.readFileSync(path.join(__dirname, 'public/tab2.mustache'), 'utf8');
   res.render('index', {
@@ -41,40 +40,31 @@ app.get('/', (req, res) => {
     tab1: tab1,
     tab2: tab2
   });  
-//   fs.readFile(templatePath, 'utf8', (err, templateHtml) => {
-//     if (err) {
-//       console.error(err); // Log the error to the console
-//       res.status(500).send('Error loading the template');
-//       return;
-//     }
-
-//     // Read tab content
-//     const tab1 = fs.readFileSync(path.join(__dirname, 'public/tab1.html'), 'utf8');
-//     const tab2 = fs.readFileSync(path.join(__dirname, 'public/tab2.html'), 'utf8');
-
-//     // Render with Mustache
-//     const rendered = mustache.render(templateHtml, { htmlContent }, {
-//       'tab-component': fs.readFileSync(path.join(__dirname, 'public/tab-component.html'), 'utf8'),
-//       'tab1': tab1,
-//       'tab2': tab2
-//     });
-
-//     res.send(rendered);
-//   });
 });
 
-// Serve markdoc files
-app.get('/docs/:language.markdoc', (req, res) => {
-  const language = req.params.language;
-  const filePath = path.join(__dirname, `docs/${language}.markdoc`);
+// Dynamic route to handle requests in the format /:file_base/:file
+app.get('/:file_base/:file', (req, res) => {
+  const fileBase = req.params.file_base;
+  const file = req.params.file;
 
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      res.status(404).send('File not found');
-      return;
-    }
-    res.type('text').send(data);
-  });
+  // Construct the file path based on the base directory and the file name
+  const filePath = path.join(__dirname, `/${fileBase}/${file}.markdoc`);
+
+  // Check if the file exists before attempting to read it
+  if (fs.existsSync(filePath)) {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        // Handle potential read errors
+        res.status(500).send('Error reading the file');
+        return;
+      }
+      // Send the file content if successfully read
+      res.type('text').send(data);
+    });
+  } else {
+    // Send a 404 error if the file does not exist
+    res.status(404).send('File not found');
+  }
 });
 
 app.listen(port, () => {
